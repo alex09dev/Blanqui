a7cc7614b48f08420f6561c3449f59deab46c80b
alex09dev <alexclaramonte@escolaculturalbadalona.com>
2025-02-19 08:15:08 +0100

    El pepe

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-const imagenes = ["/imatges/miedo.jpg", "/imatges/napoleon.png", "/imatges/japicrismas.jpg", "/imatges/porros.jpg", "/imatges/temato.jpg", "/imatges/zzz.jpg"]
-const galeria = document.querySelector(".galeria")
-const preview = document.querySelector(".preview")
-const padelante = document.querySelector(".padelante")
-const patras = document.querySelector(".patras")
-let currentPhoto = 0
-imagenes.forEach((imagen,indice) => {
+const imagenes = [
+  "/imatges/miedo.jpg",
+  "/imatges/napoleon.png",
+  "/imatges/japicrismas.jpg",
+  "/imatges/porros.jpg",
+  "/imatges/temato.jpg",
+  "/imatges/zzz.jpg",
+];
+const galeria = document.querySelector(".galeria");
+const preview = document.querySelector(".preview");
+const padelante = document.querySelector(".padelante");
+const patras = document.querySelector(".patras");
+let currentPhoto = 0;
+imagenes.forEach((imagen, indice) => {
   galeria.innerHTML += `<img id="${indice}" src="${imagen}" alt="imagen">`;
-
 });
 
 galeria.querySelectorAll("img").forEach((img) => {
   img.addEventListener("click", () => {
     preview.style.display = "flex";
     preview.querySelector("img").src = img.src;
-    preview.querySelector("img").classList.add ("pop-up")
+    preview.querySelector("img").classList.add("pop-up");
     currentPhoto = img.id;
-
+    if (likes[currentPhoto]) {
+      like.innerText = "❤";
+    } else {
+      like.innerText = "🤍";
+    }
   });
+  
 });
 
 preview.addEventListener("click", (event) => {
-    if (event.target.tagName === "IMG"|| event.target.tagName === "BUTTON") {
-        return;
-    }
-    console.log(event.target.tagName);
-    preview.style.display = "none";
+  if (event.target.tagName === "IMG" || event.target.tagName === "BUTTON") {
+    return;
+  }
+  console.log(event.target.tagName);
+  preview.style.display = "none";
+});
+const like = document.querySelector(".like");
+like.addEventListener("click", () => {
+  likes[currentPhoto] = !likes[currentPhoto];
+  console.log(likes);
+  if (likes[currentPhoto]) {
+    like.innerText = "❤";
+  } else {
+    like.innerText = "🤍";
+  }
 });
 
-padelante.addEventListener("click", () => { 
+padelante.addEventListener("click", () => {
   currentPhoto++;
   if (currentPhoto >= imagenes.length) {
     currentPhoto = 0;
@@ -35,7 +56,7 @@ padelante.addEventListener("click", () => {
   preview.querySelector("img").src = imagenes[currentPhoto];
 });
 
-patras.addEventListener("click", () => { 
+patras.addEventListener("click", () => {
   currentPhoto--;
   if (currentPhoto < 0) {
     currentPhoto = imagenes.length - 1;
@@ -43,3 +64,26 @@ patras.addEventListener("click", () => {
   preview.querySelector("img").src = imagenes[currentPhoto];
 });
 
+//looooooooooooooooooooooooooooool
+/*
+const like = document.querySelector(".like");
+
+let valoracion = localStorage.getItem("estado");
+
+like.onclick = function () {
+  valoracion = "si";
+  equisde7u7();
+};
+*/
+function equisde7u7() {
+  localStorage.setItem("estado", valoracion);
+
+  if (valoracion === "si") {
+    like.innerText = "❤";
+  }
+}
+//<button class="like">🤍</button>
+// Create an arrasy with all values at false with the same length as the images array
+const likes = Array(imagenes.length).fill(false);
+
+console.log(likes);
